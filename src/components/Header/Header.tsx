972a52c1eb23323d607103ac47982ab26a9d0a9e
MrLeon777 <valentin7gubin@gmail.com>
2024-11-05 19:26:28 +0300

    add responsive

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,19 @@
-
 import Filter from '../Filter/Filter'
 import Logo from '../Logo/Logo'
 import UpdateBtn from '../UpdateBtn/UpdateBtn'
 
 const Header = () => {
 	return (
-		<div className='p-6 bg-slate-800 flex items-center gap-10'>
-			<Logo />
-			<Filter />
-			<UpdateBtn />
+		<div className='p-6 bg-slate-800 grid sm:grid-cols-[auto_1fr_auto] grid-cols-2 items-center gap-10'>
+			<div className='row-start-1 col-start-1'>
+				<Logo />
+			</div>
+			<div className='sm:row-start-1 sm:col-start-2 row-start-2 col-span-2'>
+				<Filter />
+			</div>
+			<div className='row-start-1 sm:col-start-3 col-start-2 justify-self-end'>
+				<UpdateBtn />
+			</div>
 		</div>
 	)
 }
--- src/components/News/News.tsx
+++ src/components/News/News.tsx
@@ -25,7 +25,7 @@ const News = () => {
 	}, [refreshToggle, refetch])
 
 	return (
-		<div className='mt-10 grid grid-cols-4 gap-5'>
+		<div className='mt-10 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-5'>
 			{isLoading && <NewItemSkeleton count={12} />}
 
 			{!isLoading && data?.map(item => (
